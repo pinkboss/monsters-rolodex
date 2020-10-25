c09699d346babb2bea06f2c9eb67b6b0f2ec75fd
priyanka <priyankacv94@gmail.com>
2020-10-25 23:41:19 +0530

    Updates

---
+++ precache-manifest.ec2ad1dabe79e81b1bbcd3050f1e16db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77224b4cafe06e98d94a024d1ea532ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b535d52a7450250f8b96",
+    "url": "/monsters-rolodex/static/css/main.8adf41ba.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b535d52a7450250f8b96",
+    "url": "/monsters-rolodex/static/js/main.d15b8e1a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
